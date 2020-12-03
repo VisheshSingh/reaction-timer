eb568200432443cb5cf7f1639f9ac02bbcc7df24
Vishesh Singh <VisheshSingh16@gmail.com>
2020-12-03 13:03:53 -0600

    finishing touches

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,11 +1,32 @@
 <template>
   <p>Reaction time: {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
 </template>
 
 <script>
 export default {
   props: ['score'],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = 'Ninja fingers 🐱‍👤';
+    } else if (this.score < 400) {
+      this.rank = 'Rapid reflexes 🏇';
+    } else {
+      this.rank = 'Snail pace 🐌';
+    }
+  },
 };
 </script>
 
-<style></style>
+<style>
+.rank {
+  font-size: 1.4rem;
+  font-weight: bold;
+  color: #41b883;
+}
+</style>
